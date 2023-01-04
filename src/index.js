262de655d87b687d5c5fd41fa11fe6f0f85c1db6
Leonardo Luz <leonardoluz10@hotmail.com>
2023-01-04 14:41:04 -0300

    Criando função de help

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 import fs from 'fs';
+const caminho = process.argv
 
 function imprimirErro(error) {
     throw new Error(chalk.red(error.code, `Não há documento no diretorio!.`))
@@ -19,4 +20,17 @@ async function pegarArquivo(caminho) {
         imprimirErro(error)
     }
 }
+function help(argumento){
+    const valida = argumento[2] === "validar"
+    if (valida) {
+        console.log(chalk.yellow(`welcome to the url validation Lib`))
+        console.log(chalk.green(`commanding: npm run cli:vP  ( tests on folder files with validation.)`))
+        console.log(chalk.green(`commanding: npm run cli:P   ( tests on folder files.)`))
+        console.log(chalk.green(`commanding: npm run cli:vA  ( tests on just one file in the folder with validation.)`))
+        console.log(chalk.green(`commanding: npm run cli:vA  ( tests on just one file in the folder.`))
+        console.log(chalk.blue(`Developed by educational project`))
+    }
+    
+}
+help(caminho);
 export default pegarArquivo;
